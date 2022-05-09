004743230221c631f8be65b3cd027d0a2990ecc8
iorn <wmt.tkn.121@gmail.com>
2022-05-09 17:30:12 +0900

    model作成

--- chord/models.py
+++ chord/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Tone(models.Model):
+    order = models.IntegerField(verbose_name="順番")
+    key = models.IntegerField(verbose_name="基音")
+    octave = models.IntegerField(verbose_name="オクターブ")
+    chord = models.IntegerField(verbose_name="和音")
+    length = models.IntegerField(verbose_name="長さ")
