8207d2a2f9ce6ea1da0f438b443f364cbf03da24
iorn <wmt.tkn.121@gmail.com>
2022-05-23 13:09:28 +0900

    edit URL

--- config/settings.py
+++ config/settings.py
@@ -54,8 +54,8 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = [
-    'localhost:3000/',
-    'localhost:3000',
+    'http://localhost:3000/',
+    'http://localhost:3000',
     '127.0.0.1:3000/',
     '127.0.0.1:3000',
 ]
