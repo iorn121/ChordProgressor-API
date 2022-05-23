a52b21c0bf8187d646f81bbcf63aab43dedda9ae
iorn <wmt.tkn.121@gmail.com>
2022-05-23 12:44:02 +0900

    edit whitelist

--- config/settings.py
+++ config/settings.py
@@ -54,8 +54,10 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = [
-    'http://127.0.0.1:3000',
-    'http://localhost:3000/ChordProgressor',
+    'localhost:3000/',
+    'localhost:3000',
+    '127.0.0.1:3000/',
+    '127.0.0.1:3000',
 ]
 
 
